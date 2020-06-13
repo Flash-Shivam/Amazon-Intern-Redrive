7439f870c2ed284bd6e9dc05f02d67043e445c5d
Flash-Shivam <34097420+Flash-Shivam@users.noreply.github.com>
2020-06-13 20:50:36 +0530

    Subscription s=done , need : Lambda with SNS

--- sample1.go
+++ sample1.go
@@ -9,6 +9,7 @@ import (
   "github.com/aws/aws-sdk-go/aws/session"
   "github.com/aws/aws-sdk-go/service/sqs"
   "github.com/aws/aws-sdk-go/service/sns"
+  "github.com/aws/aws-sdk-go/service/lambda"
 )
 
 
@@ -36,6 +37,7 @@ func main() {
 
   input := flag.String("arg1","","Argument1")
   input1 := flag.String("arg2","","Argument2")
+  input2 := flag.String("arg3","","Argument3")
 
   flag.Parse()
 
@@ -90,6 +92,8 @@ result1, err1 := svc1.ListTopics(nil)
 	}
 	
 	var topic_Arn string 
+	var lambda_Arn string 
+
 
     for _, t := range result1.Topics {
 		fmt.Println(*t.TopicArn)
@@ -100,9 +104,48 @@ result1, err1 := svc1.ListTopics(nil)
 		
 	}
 
+	svc2 := lambda.New(session.New(), aws.NewConfig().WithRegion("us-east-2"))
+
+	result3, err3 := svc2.ListFunctions(nil)
+if err3 != nil {
+    fmt.Println("Cannot list functions")
+}
+
+for _, f := range result3.Functions {
+    fmt.Println("Name:        " + aws.StringValue(f.FunctionName))
+	fmt.Println("Description: " + aws.StringValue(f.Description))
+	params2 := &lambda.GetFunctionInput{
+		FunctionName: aws.String(aws.StringValue(f.FunctionName)),
+	}
+	 res,err4   := svc2.GetFunction(params2)
+	 if err4 != nil {
+	fmt.Println("Cannot list functions")
+	}
+
+	//fmt.Println(reflect.TypeOf(string(*res.Configuration.FunctionArn)))
+	
+	if strings.Contains(string(*res.Configuration.FunctionArn),*input2){
+			lambda_Arn = string(*res.Configuration.FunctionArn)
+			break
+		}
+
+}
+fmt.Println(*input1, topic_Arn, lambda_Arn)
+
+	result2, err2 := svc1.Subscribe(&sns.SubscribeInput{
+        Endpoint:              aws.String(lambda_Arn),
+        Protocol:              aws.String("lambda"),
+        ReturnSubscriptionArn: aws.Bool(true), // Return the ARN, even if user has yet to confirm
+        TopicArn:              aws.String(topic_Arn),
+    })
+    if err2 != nil {
+        fmt.Println(err.Error())
+    }
+
+    fmt.Println(*result2.SubscriptionArn)
 
 	result, err := svc1.Publish(&sns.PublishInput{
-        Message:  aws.String("Work In Progress"),
+        Message:  aws.String("Work In Progress , almost there"),
         TopicArn: aws.String(topic_Arn),
     })
     if err != nil {
